8fa7f5d0a1794fb5980cbb9cd24083aaa87f38bb
Ahmed Elwerdany <infoalwerdany@gmail.com>
2023-11-28 18:31:27 +0200

    support italy and arabic

---
+++ src/languages.ts
@@ -0,0 +1,16 @@
+const languages  = {
+    "it": {
+        "insert_image": "Inserisci immagine",
+        "from_computer": "Carica dal computer"
+    },
+    "en": {
+        "insert_image": "Insert image",
+        "from_computer": "Upload from computer"
+    },
+    "ar": {
+        "insert_image": "إدراج صورة",
+        "from_computer": "التحميل من جهاز الكمبيوتر"
+    }
+}
+
+export default languages
